6d599a0f8a578fa84ba5e56e87c017513f3f082d
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-15 16:16:18 +0530

    Added Assignment3

---
+++ Java_Learn_2/Day-2/Assinment3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Assinment3 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        int i = 1, sum = 0;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+
+    }
+}
