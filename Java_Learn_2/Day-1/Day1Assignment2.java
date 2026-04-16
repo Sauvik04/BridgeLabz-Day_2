f275c4a5421f753384f919aee1c775936ffd6610
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-16 12:58:41 +0530

    Added Day-1 Assignment 1.2

---
+++ Java_Learn_2/Day-1/Day1Assignment2.java
@@ -0,0 +1,22 @@
+public class Day1Assignment2 {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+    public static void main(String[]args){
+
+                Day1Assignment2 day = new Day1Assignment2();
+                System.out.println("Default byte value:" + day.b);
+                System.out.println("Default short value: " +day.s);
+                System.out.println("Default int value: " + day.i);
+                System.out.println("Default long value: " + day.l);
+                System.out.println("Default float value: " + day.f);
+                System.out.println("Default double value: " + day.d);
+                System.out.println("Default char value: " + day.c);
+                System.out.println("Default boolean value: " + day.bool);
+    }
+}
