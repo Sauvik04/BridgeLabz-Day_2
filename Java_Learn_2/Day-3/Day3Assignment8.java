e743c8bf333ea627e6e9fd5c3ca76c6a1dfcaedc
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-17 11:23:48 +0530

    Added Assignment

---
+++ Java_Learn_2/Day-3/Day3Assignment8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Day3Assignment8 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[]arr = new int[n];
+        for(int i =0; i < arr.length;i++){
+            arr[i] = input.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length;i++){
+            for(int j = i +1;j < arr.length;j++){
+                if(arr[i] == arr[j]){
+                    System.out.println(arr[j]);
+                }
+            }
+        }
+    }
+}
