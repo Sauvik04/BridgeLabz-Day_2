c16cbacec9ce7d8ff1079e1fbefa1a0d2f03114b
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-18 12:54:09 +0530

    Added Assignment

---
+++ Java_Learn_2/Day-6/Day6Assignment6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Day6Assignment6 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press Enter to start...");
+        sc.nextLine();
+        long start = System.currentTimeMillis();
+        System.out.println("Press Enter to stop...");
+        sc.nextLine();
+        long end = System.currentTimeMillis();
+        long elapsed = end - start;
+        System.out.println("Elapsed Time: " + elapsed + " ms");
+    }
+}
