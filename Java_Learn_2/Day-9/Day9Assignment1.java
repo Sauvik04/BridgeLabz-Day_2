4f20d76c61529231e99a7f3130903390684186c4
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-22 11:57:54 +0530

    Add UC1: Create Contact class

--- Java_Learn_2/Day-9/Day9Assignment1.java
+++ Java_Learn_2/Day-9/Day9Assignment1.java
@@ -1,4 +1,14 @@
 import java.util.*;
+class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    String phone;
+    String email;
+}
 public class Day9Assignment1 {
     public static void main(String[]args){
         System.out.println("Welcome to Address Book Program");
